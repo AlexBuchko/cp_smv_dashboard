4fd329d7660fb66beb93f090ff0b4f5d148a0183
AlexBuchko <abuchko@calpoly.edu>
2023-12-17 14:59:19 -0800

    add push route

--- src/api/hello.py
+++ src/api/hello.py
@@ -6,55 +6,78 @@ from pydantic import BaseModel
 
 
 router = APIRouter(
-    prefix="/hello",
+    prefix="/data",
     tags=["hello"],
     dependencies=[Depends(auth.get_api_key)],
 )
 
-@router.post("/hello")
-def post_hello():
-    return "hello, world!"
+class CarData(BaseModel):
+    now: float;
+    rpm: float;
+    speed: float;
+    tacho: float;
+    distance: float;
+    voltage: float;
+    temp_mos: float;
+    temp_motor: float;
+    current_motor: float;
+    current_batt: float;
+    net_energy: float;
+    efficiency: float;
+    avg_speed: float;
+    elapsed: float;
+    time_remaining: float;
+    fault_code: int;
 
+@router.post("")
+def push_data(request: CarData):
+    #TODO: Add timestamp
+    # time = request.now`
+    rpm = request.rpm
+    speed = request.speed
+    tacho = request.tacho
+    distance = request.distance
+    voltage = request.voltage
+    temp_mos = request.temp_mos
+    temp_motor = request.temp_motor
+    current_motor = request.current_motor
+    current_batt = request.current_batt
+    net_energy = request.net_energy
+    efficiency = request.efficiency
+    avg_speed = request.avg_speed
+    elapsed = request.elapsed
+    time_remaining = request.time_remaining
+    fault_code = request.fault_code 
 
-class ExampleClass(BaseModel):
-    user_id: int
-    some_value: str
-
-@router.post("/test_data")
-def post_passing_in(request: ExampleClass):
-    passed_in_id = request.user_id
-    passed_in_value = request.some_value
-    return f"passed in id: {passed_in_id}, passed in value {passed_in_value}"
-
-@router.get("/get_from_db")
-def post_get_from_db():
+    query = """
+        INSERT INTO run_history
+            (rpm, temp_motor, speed, tacho, distance, voltage, temp_mos, current_motor,
+            current_batt, net_energy, efficiency, avg_speed, elapsed, time_remaining, fault_code)
+            VALUES 
+            (:rpm, :temp_motor, :speed, :tacho, :distance, :voltage, :temp_mos, :current_motor,
+            :current_batt, :net_energy, :efficiency, :avg_speed, :elapsed, :time_remaining, :fault_code)
+    """
     with engine.begin() as connection:
-        query = text("SELECT * FROM logtable")
-        result = connection.execute(query)
-        ans = []
-        for row in result:
-            print(row)
-            ans.append(row._asdict())
-        return ans
+        connection.execute(text(query), {
+            # "time": time,
+            "rpm": rpm,
+            "speed": speed,
+            "tacho": tacho,
+            "distance": distance,
+            "voltage": voltage,
+            "temp_mos": temp_mos,
+            "temp_motor": temp_motor,
+            "current_motor": current_motor,
+            "current_batt": current_batt,
+            "net_energy": net_energy,
+            "efficiency": efficiency,
+            "avg_speed": avg_speed,
+            "elapsed": elapsed,
+            "time_remaining": time_remaining,
+            "fault_code": fault_code
+        })
+    return "success"
 
-@router.get("/get_from_db_with_binds")
-def post_get_with_binds():
-    with engine.begin() as connection:
-        query = text("SELECT * FROM logtable WHERE id = :id")
-        result = connection.execute(query, {"id": 1})
-        ans = []
-        for row in result:
-            print(row)
-            ans.append(row._asdict())
-        return ans
-
-class ExamplePost(BaseModel):
-    a: int
-    b: int
-
-@router.post("/insert_into_db")
-def post_insert(request: ExamplePost):
-    with engine.begin() as connection:
-        query = text("INSERT INTO logtable (a, b) VALUES (:a, :b)")
-        connection.execute(query, {"a": request.a, "b": request.b})
-    return "OK"
+@router.get("")
+def test():
+    return "hi"
